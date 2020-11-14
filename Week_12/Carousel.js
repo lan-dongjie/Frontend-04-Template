0e0853afe5b6241ca6c90366e1bc48b8090bb445
lan-dongjie <landongjie@github.com>
2020-11-14 19:47:39 +0800

    feat: 轮播

--- Week_12/Carousel.js
+++ Week_12/Carousel.js
@@ -3,9 +3,11 @@ import { Component } from './frameWork';
 export default class Carousel extends Component {
   constructor(type, attributes, children) {
     super(type, attributes, children);
-    this.carouselItems = [];
-    this.currentIndex = -1;
+    this.carouselItems = this.root.children;
+    this.currentIndex = 0;
+    this.isDrag = false
     this.initInterval();
+    this.initEvent();
   }
   setAttribute(name, value) {
     // this.attributes[name] = value;
@@ -17,11 +19,52 @@ export default class Carousel extends Component {
       root.appendChild(dom);
     });
   }
+  initEvent() {
+    this.root.addEventListener('mousedown', event => {
+      const { width } = this.root.getBoundingClientRect()
+      const len = this.carouselItems.length;
+      let startX = event.clientX;
+      this.isDrag = true;
+      let position = this.currentIndex
+      const move = event => {
+        const dragX = event.clientX - startX;
+        let current = position - ((dragX - dragX % width) / width)
+        for (let offset of [-1, 0, 1]) {
+          let pos = current + offset
+          pos = (pos + len) % len
+          const child = this.carouselItems[pos]
+          child.style.transition = 'none';
+          child.style.transform = `translateX(${- pos * width + offset * width + dragX % width}px)`;
+        }
+      }
+      const up = event => {
+        this.isDrag = false
+        const dragX = event.clientX - startX;
+        position = position - Math.round(dragX / width);
+        for (let offset of [0, - Math.sign(Math.round(dragX / width) - dragX + width / 2 * Math.sign(dragX))]) {
+          let pos = position + offset
+          pos = (pos + len) % len
+          if (offset === 0) {
+            this.currentIndex = pos
+          }
+          const child = this.carouselItems[pos]
+          child.style.transition = '';
+          child.style.transform = `translateX(${- pos * width + offset * width}px)`;
+        }
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', up)
+      }
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)
+    })
+  }
   initInterval() {
-    this.currentIndex = 0;
-    this.carouselItems = this.root.children;
+
     const len = this.carouselItems.length;
     setInterval(() => {
+      if (this.isDrag) {
+        return
+      }
       let currentIndex = this.currentIndex % len;
       let current = this.carouselItems[currentIndex];
 
