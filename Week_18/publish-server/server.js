efeb5ecb9ccf461204474b29ad6ed434a6ea4e3d
lan-dongjie <landongjie@github.com>
2020-12-23 23:54:31 +0800

    feat: git app

--- Week_18/publish-server/server.js
+++ Week_18/publish-server/server.js
@@ -1,16 +1,108 @@
 let http = require('http')
-let fs = require('fs')
+let https = require('https')
 
-
-http.createServer(function (request, response) {
-  // console.log('req', request.headers);
-  let outFile = fs.createWriteStream("../server/public/index.html")
-  request.on('data', chunk => {
-    console.log('chunk', chunk);
-    outFile.write(chunk)
+// let fs = require('fs')
+let querystring = require('querystring')
+let unzipper = require('unzipper')
+//用code  换token
+function auth(request, response) {
+  let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1])
+  console.log('query', query);
+  if (!query.code) {
+    response.end('404')
+    return
+  }
+  getToken(query.code, function (info) {
+    console.log('---', info);
+    // response.write(JSON.stringify(info))
+    response.write(`<a href="http://localhost:8083?token=${info.token}">publish</a>`)
+    response.end()
   })
-  request.on('end', () => {
-    outFile.end()
+}
+//  https://github.com/login/oauth/access_token
+function getToken(code, callback) {
+  console.log('code', code);
+  let request = https.request({
+    hostname: "github.com",
+    path: `/login/oauth/access_token?code=${code}&client_id=Iv1.af021f302b437824&client_secrets=3ec8201d268388d4d8e40ec6b9c7f130bcd90fcd`,
+    port: 443,
+    method: 'POST'
+  }, function (response) {
+    let body = ''
+    response.on("data", chunk => {
+      body += chunk.toString()
+    })
+    response.on("end", chunk => {
+      callback(querystring.parse(body))
+      response.end()
+    })
+  })
+  // request.end()
+}
+function getUser(token, callback) {
+  let request = https.request({
+    hostname: "api.github.com",
+    path: `/user`,
+    port: 443,
+    method: 'GET',
+    headers: {
+      "Authorization": `token ${token}`,
+      "User-Agent": "ldj-toy-app",
+    }
+  }, function (response) {
+    let body = ''
+    response.on("data", chunk => {
+      body += chunk.toString()
+    })
+    response.on("end", chunk => {
+      callback(querystring.parse(body))
+    })
+  })
+  request.end()
+}
+function publish(request, response) {
+  let query = querystring.parse(request.url.match(/^\/publish\?([\s\S]+)$/)[1])
+  console.log('request', request.url);
+  response.end()
+  return
+  getUser(query.token, info => {
+    console.log('--info-', info);
+    if (info.name === 'lan-dongjie') {
+      upload(request, response)
+    }
+  })
+
+}
+
+function upload(request, response) {
+  // console.log('request', request);
+  request.pipe(unzipper.Extract({ path: '../server/public/' }))
+  request.on('end', function () {
     response.end('success')
   })
+}
+http.createServer(function (request, response) {
+  // console.log('req', request.headers);
+  // let outFile = fs.createWriteStream("../server/public/index.html")
+  // let outFile = fs.createWriteStream("../server/public/tmp.zip")
+
+  // request.pipe(outFile)
+  // request.on('data', chunk => {
+  //   outFile.write(chunk)
+  // })
+  // request.on('end', () => {
+  //   outFile.end()
+  //   response.end('success')
+  // })
+  // request.pipe(unzipper.Extract({ path: '../server/publish/' }))
+  if (request.url === '/upload') {
+    return publish(request, response)
+  }
+  if (request.url.match(/^\/auth\?/)) {
+    return auth(request, response)
+  }
+  if (request.url.match(/^\/publish\?/)) {
+    return publish(request, response)
+  }
+  response.end('404')
 }).listen(8082)
--- Week_18/tool/publish.js
+++ Week_18/tool/publish.js
@@ -1,23 +1,72 @@
 let http = require('http')
-let fs = require('fs')
-let file = fs.createReadStream('./sample.html')
+// let fs = require('fs')
+let child_process = require('child_process')
+let archiver = require('archiver')
+let querystring = require('querystring')
 
-let req = http.request({
-  hostname: "127.0.0.1",
-  port: 8082,
-  method: "POST",
-  header: {
-    "Content-Type": "application/octet-stream",
+//  https://github.com/login/oauth/authorize
+child_process.exec(`start https://github.com/login/oauth/authorize?client_id=Iv1.af021f302b437824`)
+
+
+const archive = archiver('zip', {
+  zlib: { level: 9 } // Sets the compression level.
+});
+archive.directory('./sample/', false);
+// archive.pipe(fs.createWriteStream('tmp.zip'))
+// archive.finalize()
+
+// fs.stat('./sample.html', (err, stats) => {
+// let request = http.request({
+//   hostname: "127.0.0.1",
+//   port: 8082,
+//   method: "POST",
+//   path: '/upload',
+//   header: {
+//     "Content-Type": "application/octet-stream",
+//     // "Content-Length": stats.size
+//   }
+// }, response => {
+//   // console.log(response);
+// })
+
+// archive.pipe(request)
+// archive.finalize()
+
+// let file = fs.createReadStream('./sample.html')
+// file.pipe(request)
+// file.on('end', () => {
+//   request.end()
+// })
+// })
+
+// file.on('data', chunk => {
+//   request.write(chunk)
+// })
+// file.on('end', () => {
+//   request.end()
+// })
+
+http.createServer(function (request, response) {
+  let query = querystring.parse(request.url.match(/^\?([\s\S]+)$/)[1])
+  console.log('token', token, request.url);
+  if (!token) {
+    response.end('error')
+    return
   }
-}, response => {
-  console.log(response);
-})
+  publish(query.token)
+}).listen(8083)
 
-file.on('data', chunk => {
-  console.log(chunk.toString());
-  req.write(chunk)
-})
-file.on('end', () => {
-  // console.log(chunk.toString());
-  req.end()
-})
+function publish(token) {
+  let request = http.request({
+    hostname: "127.0.0.1",
+    port: 8082,
+    method: "POST",
+    path: `/publish?token=${token}`,
+    header: {
+      "Content-Type": "application/octet-stream",
+      // "Content-Length": stats.size
+    }
+  }, response => {
+    console.log(response);
+  })
+}
